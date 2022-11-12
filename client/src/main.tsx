42fa00bbc377211b94e17170fae96b9268c946bd
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-11-12 17:45:52 +0900

    feat: create Home & Auth components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GlobalStyle } from "./styles/globalStyle";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
